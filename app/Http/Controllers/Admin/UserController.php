e2f9778e8c59b7eb4296802b46a4a7a191012b95
Syahrizal Alisadikin <56682176+syahrizal-alisadikin@users.noreply.github.com>
2022-04-12 22:08:44 +0700

    Update storage

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
 use DataTables;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
