8a3b9987c5e96c057f7b7e03a9c7e6f7b025f3f6
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-04-15 10:39:21 +0700

    Dashboard admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        if(Auth::user()->roles == "ADMIN"){
+            $quantity = Order::where('status' != "PENDING")->sum('quantity');
+            $totalMonth = Order::where('status' != "PENDING")->whereMonth('created_at',date('m'))->sum('total_harga');
+            $totalYears = Order::where('status' != "PENDING")->whereYear('created_at',date('Y'))->sum('total_harga');
+        }else{
+            $quantity = Order::where('user_id',Auth::user()->id)->sum('quantity');
+            $totalMonth = Order::where('user_id',Auth::user()->id)->where('status' != "PENDING")->whereMonth('created_at',date('m'))->sum('total_harga');
+            $totalYears = Order::where('user_id',Auth::user()->id)->where('status' != "PENDING")->whereYear('created_at',date('Y'))->sum('total_harga');
+
+        }
+        return view('admin.dashboard',compact('quantity','totalMonth','totalYears'));
     }
 
    
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,16 +8,15 @@
   </div>
 </div>
 <div class="row">
-    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+    <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
       <div class="card">
         <div class="card-body p-3">
           <div class="row">
             <div class="col-8">
               <div class="numbers">
-                <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Money</p>
+                <p class="text-sm mb-0 text-capitalize font-weight-bold">Penjualan</p>
                 <h5 class="font-weight-bolder mb-0">
-                  $53,000
-                  <span class="text-success text-sm font-weight-bolder">+55%</span>
+                  {{ $quantity }} Ekor
                 </h5>
               </div>
             </div>
@@ -30,16 +29,15 @@
         </div>
       </div>
     </div>
-    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+    <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
       <div class="card">
         <div class="card-body p-3">
           <div class="row">
             <div class="col-8">
               <div class="numbers">
-                <p class="text-sm mb-0 text-capitalize font-weight-bold">Today's Users</p>
+                <p class="text-sm mb-0 text-capitalize font-weight-bold">Penjualan Bulan {{ date('M') }}</p>
                 <h5 class="font-weight-bolder mb-0">
-                  2,300
-                  <span class="text-success text-sm font-weight-bolder">+3%</span>
+                  {{ moneyFormat($totalMonth) }}
                 </h5>
               </div>
             </div>
@@ -52,16 +50,15 @@
         </div>
       </div>
     </div>
-    <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+    <div class="col-xl-4 col-sm-6 mb-xl-0 mb-4">
       <div class="card">
         <div class="card-body p-3">
           <div class="row">
             <div class="col-8">
               <div class="numbers">
-                <p class="text-sm mb-0 text-capitalize font-weight-bold">New Clients</p>
+                <p class="text-sm mb-0 text-capitalize font-weight-bold">Penjualan Tahun {{ date('Y') }}</p>
                 <h5 class="font-weight-bolder mb-0">
-                  +3,462
-                  <span class="text-danger text-sm font-weight-bolder">-2%</span>
+                  {{ moneyFormat($totalYears) }}
                 </h5>
               </div>
             </div>
@@ -74,28 +71,7 @@
         </div>
       </div>
     </div>
-    <div class="col-xl-3 col-sm-6">
-      <div class="card">
-        <div class="card-body p-3">
-          <div class="row">
-            <div class="col-8">
-              <div class="numbers">
-                <p class="text-sm mb-0 text-capitalize font-weight-bold">Sales</p>
-                <h5 class="font-weight-bolder mb-0">
-                  $103,430
-                  <span class="text-success text-sm font-weight-bolder">+5%</span>
-                </h5>
-              </div>
-            </div>
-            <div class="col-4 text-end">
-              <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                <i class="ni ni-cart text-lg opacity-10" aria-hidden="true"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+   
   </div>
 
  
