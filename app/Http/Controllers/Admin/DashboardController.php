5ad9ed50b0eeec58f3b5132aebb96b1f0127c432
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-04-15 10:41:15 +0700

    Dashboard admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
