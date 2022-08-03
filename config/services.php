6eef8ba7d8da223dd1ea432f7b417fb34f6eff71
Syahrizal Alisadikin <56682176+syahrizal-alisadikin@users.noreply.github.com>
2022-08-03 17:39:52 +0700

    Update services.php

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://aqiqah.izaldev.my.id/auth/google/callback',
+        'redirect' => 'https://saungaqiqah.izaldev.my.id/auth/google/callback',
     ],
 
 ];
