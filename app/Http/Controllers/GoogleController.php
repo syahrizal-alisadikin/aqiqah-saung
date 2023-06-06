9d1118deeea085908dd939fb60e837c7ac8919fc
izal <alisadikinsyahrizal@gmail.com>
2023-06-06 21:55:24 +0700

    update google

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -15,7 +15,7 @@ class GoogleController extends Controller
         $client = new \Google_Client([
             'client_id' => config('services.google.client_id')
         ]);
-        dd($idToken . " - " . $request->all());
+        dd($idToken);
         $payload = $client->verifyIdToken($idToken);
 
         if (!$payload) {
